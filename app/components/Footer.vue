143bfbaabcc97cf44d4498ab96bcff2e79aa0135
Christian Eckardt <christian.eckardt@tchibo.de>
2025-10-25 12:22:06 +0200

    add footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <UFooter>
+    <template #left>
+      <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }}</p>
+    </template>
+
+    <UNavigationMenu :items="items" variant="link" />
+
+    <template #right>
+      <UButton
+          icon="i-simple-icons-github"
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/EckeEcke"
+          target="_blank"
+          aria-label="Github"
+      />
+      <UButton
+          icon="i-simple-icons-linkedin"
+          color="neutral"
+          variant="ghost"
+          to="https://www.linkedin.com/in/christian-eckardt-41527216a/"
+          target="_blank"
+          aria-label="LinkedIn"
+      />
+    </template>
+  </UFooter>
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[] = [
+  {
+    label: 'Portfolio',
+    to: 'https://eckeecke.github.io/',
+    target: '_blank'
+  },
+  {
+    label: 'About',
+    to: '/about',
+  }
+]
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
   <slot />
+  <Footer />
   <div class="fixed bottom-4 right-4">
     <UColorModeButton />
   </div>
 </template>
 
 <script setup lang="ts">
+import Footer from "~/components/Footer.vue";
 </script>
